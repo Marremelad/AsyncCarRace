1c4022f888edd50d83ef2b0872716682268dbfc7
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-21 17:24:33 +0200

    Implemented some logic to method

--- AsyncCarRace/Events.cs
+++ AsyncCarRace/Events.cs
@@ -1,6 +1,22 @@
 ﻿namespace AsyncCarRace;
 
-public class Events
+public static class Events
 {
-    
+    private static readonly object LockEvent = new object();
+    public static void RandomEvent(Car car)
+    {
+        int randomEvent = new Random().Next(1, 101);
+
+        if (randomEvent is > 0 and < 11)
+        {
+            Console.WriteLine("Random event 1");
+            car.Speed = 100;
+
+        }
+        else if (randomEvent is > 10 and < 101)
+        {
+            Console.WriteLine("Random event 2");
+            car.Speed = 200;
+        }
+    }
 }
