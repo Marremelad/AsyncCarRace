dfcd001c78a4246d82b2e3d16190a7a7e4a2aae0
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-23 09:40:54 +0200

    Changed speed property into field with default value

--- AsyncCarRace/Car.cs
+++ AsyncCarRace/Car.cs
@@ -3,13 +3,12 @@
 public class Car
 {
     private string? Name { get; set; }
-    public int Speed { get; set; }
+    private int _speed = 120;
     private readonly double _distanceTraveled = 0;
     
 
-    public Car(string name, int sped)
+    public Car(string name)
     {
         Name = name;
-        Speed = sped;
     }
 }
