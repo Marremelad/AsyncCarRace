4af6eea193ad6003d7d69baf81e664d9942d99c1
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-23 09:24:52 +0200

    Implemented basics of Events class

--- AsyncCarRace/Events.cs
+++ AsyncCarRace/Events.cs
@@ -2,5 +2,15 @@
 
 public static class Events
 {
-    
+    public enum AvailableEvents
+    {
+        FlatTire = 100,
+        OutOfGas = 50,
+        Crash = 0,
+    }
+
+    public static void GetEvent()
+    {
+        
+    }
 }
