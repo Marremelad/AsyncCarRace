9c64cb01e1261ab652e658b4e297b012d58113c7
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-23 09:18:00 +0200

    Cleaned up file

--- AsyncCarRace/Car.cs
+++ AsyncCarRace/Car.cs
@@ -2,14 +2,5 @@
 
 public class Car
 {
-    public string Name { get; set; }
-    public int Speed { get; set; }
-    public double DistanceTraveled;
-    public int Placement;
-
-    public Car(string name, int speed)
-    {
-        Name = name;
-        Speed = speed;
-    }
+    
 }
