529f7f4a22834502dcdafb88701dd366fb9965c3
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-23 09:18:36 +0200

    Cleaned up file

--- AsyncCarRace/Events.cs
+++ AsyncCarRace/Events.cs
@@ -2,21 +2,5 @@
 
 public static class Events
 {
-    private static readonly object LockEvent = new object();
-    public static void RandomEvent(Car car)
-    {
-        int randomEvent = new Random().Next(1, 101);
-
-        if (randomEvent is > 0 and < 11)
-        {
-            Console.WriteLine("Random event 1");
-            car.Speed = 100;
-
-        }
-        else if (randomEvent is > 10 and < 101)
-        {
-            Console.WriteLine("Random event 2");
-            car.Speed = 200;
-        }
-    }
+    
 }
