18d9515b23fb09773343c44322424937dad28085
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-23 09:24:38 +0200

    Implemented basics of Car class

--- AsyncCarRace/Car.cs
+++ AsyncCarRace/Car.cs
@@ -2,5 +2,15 @@
 
 public class Car
 {
+    public string? Name { get; set; }
+    public int Speed { get; set; }
+    public double DistanceTraveled { get; set; }
     
+
+    public Car(string name, int sped, double distanceTraveled)
+    {
+        Name = name;
+        Speed = sped;
+        DistanceTraveled = distanceTraveled;
+    }
 }
