34c7b32027b582e654b1b66741fcf43cafeca7d1
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-23 09:41:27 +0200

    Created loop to make threads and cars

--- AsyncCarRace/Race.cs
+++ AsyncCarRace/Race.cs
@@ -2,5 +2,30 @@
 
 public static class Race
 {
-    
+    public static List<Car>? Podium { get; set; }
+    public static void Run()
+    {
+        List<Car> cars = new List<Car>()
+        {
+            new Car("Lightning McQueen"),
+            new Car("Chick Hicks"),
+            new Car("Strip Weathers"),
+        };
+
+        for (int i = 0; i < 3; i++)
+        {
+            Car car = cars[i];
+            Thread thread = new Thread(() => Go(car));
+            thread.Start();
+        }
+        
+    }
+
+    public static void Go(Car car)
+    {
+        while (true)
+        {
+            
+        }
+    }
 }
