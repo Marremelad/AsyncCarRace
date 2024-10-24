5296b718d8f84c18bb3afeae1a70baf4a63c47e9
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-24 14:23:11 +0200

    Added fields PitStop and HasCrashed

--- AsyncCarRace/Car.cs
+++ AsyncCarRace/Car.cs
@@ -5,7 +5,9 @@ public class Car
     public string? Name { get;}
     public int Speed = 300;
     public double DistanceTraveled = 0;
+    public bool PitStop = false;
     public bool FinishedRace = false;
+    public bool HasCrashed = false;
     
     public Car(string name)
     {
